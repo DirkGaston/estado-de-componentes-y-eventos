bcb7a6d93492f7316b9b65abed56a1f7ed821c3e
DirkGaston <dirkgaston87@gmail.com>
2022-08-17 20:45:00 -0400

    componentes visuales y eventHandlers

---
+++ src/components/Button.jsx
@@ -0,0 +1,16 @@
+const Button = () => {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <p className="mb-3">Contraseña correcta!</p>
+      <p className="mb-3 text-red-500">Pulsa el botón bajo tu propia responsabilidad...</p>
+      <button
+        type="button"
+        class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+      >
+        Red
+      </button>
+    </div>
+  );
+};
+
+export default Button;
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,3 +1,43 @@
-const Input = () => {};
+const Input = () => {
+  const handleChange = (e) => {
+    console.log(e.target.value);
+  };
+  return (
+    <div className="inputContainer">
+      <div class="mb-6">
+        <label
+          for="name"
+          class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+        >
+          Nombre
+        </label>
+        <input
+          onChange={handleChange}
+          type="name"
+          id="name"
+          class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+          placeholder="Ingresa tu nombre"
+          required
+        />
+      </div>
+      <div class="mb-6">
+        <label
+          for="password"
+          class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+        >
+          Contraseña
+        </label>
+        <input
+          onChange={handleChange}
+          type="password"
+          id="password"
+          class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+          placeholder="Ingresa una contraseña"
+          required
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Input;
